cf6f5cb6d83d8f629b484c4b63529c6e201b1324
Patrick <patrickjcor@gmail.com>
2021-03-18 14:24:18 +0800

    Pre-train CNN

---
+++ models.py
@@ -0,0 +1,233 @@
+from math import floor, ceil
+import os
+
+import dill
+from sklearn.model_selection import train_test_split
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision.datasets import ImageFolder
+from torchvision import transforms
+
+
+classes = ['a', 'ba', 'dara', 'ei', 'ga', 'ha', 'ka', 'kuw', 'la', 'ma', 'na', 'nga', 'ou', 'pa', 'sa', 'ta', 'tul', 'wa', 'ya']
+
+def same_pad_values(dim, kernel_size, stride=(1, 1)):
+    total_h_pad = stride[0]*dim[0]-dim[0]+kernel_size[0]-stride[0]
+    smaller_h_pad = floor(total_h_pad / 2)
+    larger_h_pad = ceil(total_h_pad / 2)
+    total_v_pad = stride[1]*dim[1]-dim[1]+kernel_size[1]-stride[1]
+    smaller_v_pad = floor(total_v_pad / 2)
+    larger_v_pad = ceil(total_v_pad / 2)
+    return (smaller_h_pad, larger_h_pad, smaller_v_pad, larger_v_pad)
+
+
+def find_new_dim(dim, kernel_size, stride=(1, 1), pad=(0, 0, 0, 0)):
+    return (floor((dim[0]+sum(pad[:2])-(kernel_size[0]-1)-1)/stride[0]+1),
+          floor((dim[1]+sum(pad[2:])-(kernel_size[1]-1)-1)/stride[1]+1))
+
+
+class baybayin_net(nn.Module):
+    def __init__(self, args):
+        super(baybayin_net, self).__init__()
+
+        if args['activation_fn'] == 'Sigmoid':
+                activation_fn = nn.Sigmoid()
+        elif args['activation_fn'] == 'Tanh':
+            activation_fn = nn.Tanh()
+        elif args['activation_fn'] == 'ReLU':
+            activation_fn = nn.ReLU()
+        
+        self.conv_layers = []
+        dim = (32, 32)
+        for i, conv_layer in enumerate(args['conv_layer_configs']):
+            if conv_layer['padding'] == 'same':
+                self.conv_layers.append(
+                    nn.ZeroPad2d(
+                        same_pad_values(
+                            dim, conv_layer['kernel_size'], conv_layer['stride']
+                        )
+                    )
+                )
+            else:
+                dim = find_new_dim(dim, conv_layer['kernel_size'], conv_layer['stride'])
+      
+            self.conv_layers.append(
+                nn.Conv2d(
+                  in_channels=(3 if i==0 else args['conv_layer_configs'][i-1]['filters']),
+                  out_channels=conv_layer['filters'],
+                  kernel_size=conv_layer['kernel_size'],
+                  stride=conv_layer['stride']
+                )
+            )
+
+            if args['batch_norm']:
+                self.conv_layers.append(nn.BatchNorm2d(conv_layer['filters']))
+      
+            self.conv_layers.append(nn.MaxPool2d(kernel_size=conv_layer['pool']))
+            dim = find_new_dim(dim, conv_layer['pool'], (2, 2))
+    
+        self.conv_layers = nn.ModuleList(self.conv_layers)
+        
+        self.fc_layers = []
+        num_of_fc_layers = len(args['fc_layer_configs'])
+        for i, fc_layer_config in enumerate(args['fc_layer_configs']):
+            self.fc_layers.append(
+                nn.Linear(
+                    dim[0]*dim[1]*args['conv_layer_configs'][-1]['filters'] if i==0 else args['fc_layer_configs'][i-1]['size'],
+                    fc_layer_config['size']
+                )
+            )
+            self.fc_layers.append(activation_fn)
+            self.fc_layers.append(nn.Dropout(args['dropout']))
+        self.fc_layers.append(
+            nn.Linear(args['fc_layer_configs'][-1]['size'], 19)
+            )
+        self.fc_layers = nn.ModuleList(self.fc_layers)
+  
+    def forward(self, x):
+        b = x.shape[0]
+        for layer in self.conv_layers:
+            x = layer(x)
+        x = x.view(b, -1)
+        for layer in self.fc_layers:
+            x = layer(x)
+        return x
+
+def pre_process_image(image):
+    return transforms.Compose([
+                            transforms.ToTensor(),
+                            transforms.Lambda(
+                                lambda x : transforms.Resize(
+                                    round(min(x.shape[-2:]) * 32 / max(x.shape[-2:]))
+                                )(x)
+                            ), # resize so larger dimension is 32
+                            transforms.Pad(16, fill=1), # pad dimensions in case current dimensions are less than 32x32
+                            transforms.CenterCrop(32), # crop to 32x32
+                            transforms.Lambda(
+                                lambda x : x.expand(3, -1, -1)
+                            ),
+                            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) # normalize between 1 and -1
+    ])(image)
+
+def get_transforms():
+    return transforms.Compose([
+                            transforms.ToTensor(),
+                            transforms.Lambda(
+                                lambda x : transforms.Resize(
+                                    round(min(x.shape[-2:]) * 32 / max(x.shape[-2:]))
+                                )(x)
+                            ), # resize so larger dimension is 32
+                            transforms.Pad(16, fill=1), # pad dimensions in case current dimensions are less than 32x32
+                            transforms.CenterCrop(32), # crop to 32x32
+                            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) # normalize between 1 and -1
+    ])
+
+def train_model(model, save_path, args):
+
+    print(model)
+
+    tfs = get_transforms()
+  
+    print('Creating dataset...', end='')
+    if os.path.isfile('baybayin_images.pickle'):
+        # load baybayin images dataset from file if it exists
+        with open('baybayin_images.pickle', 'rb') as file:
+            baybayin_images = dill.load(file)
+    else:
+        # create baybayin images and save it to a file
+        baybayin_images = ImageFolder(root='./Baybayin-Handwritten-Character-Dataset/raw', transform=tfs)
+        with open('baybayin_images.pickle', 'wb') as file:
+            dill.dump(baybayin_images, file)
+    print('Done')
+
+    # split images into train and test sets
+    print('Creating train and test sets...', end='')
+    if os.path.isfile('baybayin_train.picke') and os.path.isfile('baybayin_test.pickle'):
+        with open('baybayin_train.pickle', 'rb') as file:
+            baybayin_train = dill.load(file)
+        with open('baybayin_test.picke', 'rb') as file:
+            baybayin_test = dill.load(file)
+    else:
+        baybayin_train, baybayin_test = train_test_split(baybayin_images, train_size=0.7, stratify=([y for _, y in baybayin_images]), random_state=42)
+        with open('baybayin_train.pickle', 'wb') as file:
+            dill.dump(baybayin_train, file)
+        with open('baybayin_test.picke', 'wb') as file:
+            dill.dump(baybayin_test, file)
+    print('Done')
+
+    # create train and test loaders
+    baybayin_trainloader = torch.utils.data.DataLoader(baybayin_train, batch_size=args['batch_size'], shuffle=True, num_workers=2)
+    baybayin_testloader = torch.utils.data.DataLoader(baybayin_test, batch_size=args['batch_size'], shuffle=True, num_workers=2)
+
+    # initialize optimizer
+    if args['optimizer_class'] == 'SGD':
+        optimizer = optim.SGD(model.parameters(), lr=args['learning_rate'], momentum=args['momentum'])
+    elif args['optimizer_class'] == 'Adam':
+        optimizer = optim.Adam(model.parameters(), lr=args['learning_rate'], betas=args['betas'])
+
+    criterion = nn.CrossEntropyLoss() # initialize loss
+
+    for epoch in range(args['epochs']):  # loop over the dataset multiple times
+
+        running_loss = 0
+        for i, data in tqdm(enumerate(baybayin_trainloader, 0), desc=f'Epoch {epoch}'):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print(f'epoch {epoch:2d}\titer {i:5d}\trunning loss {running_loss/2000:.3f} %.3f')
+                running_loss = 0.0
+    torch.save(model.state_dict(), save_path)
+    print('Finished Training')
+
+
+DEFAULT_CNN_PARAMS = {
+'conv_layer_configs' : [
+                        {'filters' : 6,
+                        'kernel_size' : (5, 5),
+                        'stride' : (1, 1),
+                        'pool' : (2, 2),
+                        'padding' : 'valid'},
+                        {'filters' : 16,
+                        'kernel_size' : (5, 5),
+                        'stride' : (1, 1),
+                        'pool' : (2, 2),
+                        'padding' : 'valid'}
+                    ],
+'fc_layer_configs' : [
+                        {'size' : 120},
+                        {'size' : 84},
+                    ],
+'batch_norm' : False,
+'dropout' : 0.0,
+'activation_fn' : 'ReLU'
+}
+
+DEFAULT_TRAIN_PARAMS = {
+    'epochs' : 2,
+    'batch_size' : 4,
+    'optimizer_class' : 'SGD',
+    'learning_rate' : 0.001,
+    'momentum' : 0.9
+}
+
+# foo = baybayin_net(DEFAULT_CNN_PARAMS)
+# print(foo)
+# print(foo(torch.randn(1, 3, 32, 32)))
+# train_model(foo, 'test.pt', DEFAULT_TRAIN_PARAMS)
+# bar = baybayin_net(DEFAULT_CNN_PARAMS)
+# print(bar)
+# bar.load_state_dict(torch.load('test.pt'))
