4a3eaa483ac9e2be0032f5eb52d40a860f0cfb43
Temujin U. Lampasa <temujin.lampasa@obf.ateneo.edu>
2021-03-23 02:09:52 +0800

    Update forms.py
    - Set form labels

--- forms.py
+++ forms.py
@@ -15,25 +15,25 @@ class XY_Form(FlaskForm):
     y = IntegerField('y', validators=[InputRequired(), NumberRange(min=0, max=10, message="error")])
 
 class CNNForm(FlaskForm):
-    filters = IntegerField('filters', validators=[InputRequired(), NumberRange(0, 10)])
+    filters = IntegerField('Filters', validators=[InputRequired(), NumberRange(0, 10)])
     kernel = FormField(XY_Form)
     stride = FormField(XY_Form)
 
-    conv_layer_on = BooleanField('conv_on')
-    padding = SelectField('Padding:', choices=["same", "valid"], validators=[InputRequired()])
+    conv_layer_on = BooleanField('On')
+    padding = SelectField('Padding', choices=["same", "valid"], validators=[InputRequired()])
     pool_size = FormField(XY_Form)
     batch_norm = BooleanField('Batch Norm')
 
-    output_size = IntegerField('output_size', validators=[InputRequired()])
-    dropout = DecimalField('dropout', places=1, widget=NumberInput(step=0.1), validators=[InputRequired()])
-    activation = SelectField('activation', choices=['sigmoid', 'tanh', 'ReLu'], validators=[InputRequired()])
-    dense_layer_on = BooleanField('dense_on')
+    output_size = IntegerField('Output Size', validators=[InputRequired()])
+    dropout = DecimalField('Dropout', places=1, widget=NumberInput(step=0.1), validators=[InputRequired()])
+    activation = SelectField('Activation Function', choices=['sigmoid', 'tanh', 'ReLu'], validators=[InputRequired()])
+    dense_layer_on = BooleanField('On')
 
 
 class RetrainModelForm(FlaskForm):
-    optimizer = SelectField('optimizer', choices=['SGD', 'Adam', 'Nadam', 'RMSProp'])
-    learning_rate = DecimalField('dropout', places=2, widget=NumberInput(step=0.01))
-    beta1 = DecimalField('beta1', places=2, widget=NumberInput(step=0.01))
-    beta2 = DecimalField('beta2', places=2, widget=NumberInput(step=0.01))
-    batch_size = IntegerField('batch_size')
-    epochs = IntegerField('epochs')
+    optimizer = SelectField('Optimizer', choices=['SGD', 'Adam', 'Nadam', 'RMSProp'])
+    learning_rate = DecimalField('Dropout', places=2, widget=NumberInput(step=0.01))
+    beta1 = DecimalField('Beta 1', places=2, widget=NumberInput(step=0.01))
+    beta2 = DecimalField('Beta 2', places=2, widget=NumberInput(step=0.01))
+    batch_size = IntegerField('Batch Size')
+    epochs = IntegerField('Epochs')
