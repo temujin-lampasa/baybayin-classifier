cd0c7406481616c3e056a2b9be36757eb1b56cb7
Temujin U. Lampasa <temujin.lampasa@obf.ateneo.edu>
2021-03-24 20:45:09 +0800

    Hide unselected layers
    - Add javascript to hide layers that weren't selected.
    
    Issue: error during form validation when form is not focusable.

--- static/index.js
+++ static/index.js
@@ -1,12 +1,33 @@
 var conv_buttons = document.getElementsByClassName("convLayerBtn")
 var fc_buttons = document.getElementsByClassName("fcLayerBtn")
 
-for (let btn of conv_buttons) {
-    btn.onclick = function() {
-        for (let btn2 of conv_buttons) {
-            btn2.disabled = false;
+const NUM_LAYERS = 4;
+layers = [];
+
+for (let i=1; i<=NUM_LAYERS; i++) {
+    layers.push(document.getElementsByClassName("cnn-layer"+i))
+}
+
+
+for (let i=0; i< NUM_LAYERS; i++){
+    conv_buttons[i].onclick = function() {
+
+        // Disable this button. Enable all other buttons.
+        for (let j=0; j<NUM_LAYERS; j++) {
+            conv_buttons[j].disabled = false;
         }
-        btn.disabled = true;
+        conv_buttons[i].disabled = true;
+
+        // Make the layer i visible. Make all other layers invisible.
+        for (let j=0; j<NUM_LAYERS; j++) {
+            let display = ""
+            if (j!=i) {
+                display = "none"
+            }
+            for (let elem of layers[j]) {
+                elem.style.display = display;
+            }
+        }
     }
 }
 
