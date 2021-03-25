6798eba6454ca4345fca21c457fede7d1a22dd01
Temujin U. Lampasa <temujin.lampasa@obf.ateneo.edu>
2021-03-25 15:56:53 +0800

    Add layer switching to fc layers
    - refactor javascript for setting btn functions

--- static/index.js
+++ static/index.js
@@ -2,39 +2,35 @@ var conv_buttons = document.getElementsByClassName("convLayerBtn")
 var fc_buttons = document.getElementsByClassName("fcLayerBtn")
 
 const NUM_LAYERS = 4;
-layers = [];
+conv_layers = [];
+fc_layers = [];
 
 for (let i=1; i<=NUM_LAYERS; i++) {
-    layers.push(document.getElementsByClassName("conv-layer"+i))
+    conv_layers.push(document.getElementsByClassName("conv-layer"+i))
+    fc_layers.push(document.getElementsByClassName("fc-layer"+i))
 }
 
 
-for (let i=0; i< NUM_LAYERS; i++){
-    conv_buttons[i].onclick = function() {
+function setBtnOnClick(buttons, layers) {
+    for (let i=0; i< NUM_LAYERS; i++){
+        buttons[i].onclick = function() {
 
-        // Disable this button. Enable all other buttons.
-        for (let j=0; j<NUM_LAYERS; j++) {
-            conv_buttons[j].disabled = false;
-        }
-        conv_buttons[i].disabled = true;
+            // Disable this button. Enable all other buttons.
+            for (let j=0; j<NUM_LAYERS; j++) {
+                buttons[j].disabled = false;
+            }
+            buttons[i].disabled = true;
 
-        // Make the layer i visible. Make all other layers invisible.
-        for (let j=0; j<NUM_LAYERS; j++) {
-            let display = (j!=i) ? "none": ""
-            for (let elem of layers[j]) {
-                elem.style.display = display;
+            // Make the layer i visible. Make all other layers invisible.
+            for (let j=0; j<NUM_LAYERS; j++) {
+                let display = (j!=i) ? "none": ""
+                for (let elem of layers[j]) {
+                    elem.style.display = display;
+                }
             }
         }
     }
 }
 
-for (let btn of fc_buttons) {
-    btn.onclick = function() {
-        for (let btn2 of fc_buttons) {
-            btn2.disabled = false;
-        }
-        btn.disabled = true;
-    }
-}
-
-
+setBtnOnClick(conv_buttons, conv_layers);
+setBtnOnClick(fc_buttons, fc_layers);
